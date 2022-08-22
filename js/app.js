cfc7e5210cbcc08dd64c69274f8584a7185d9dd6
Achintya151 <achintyakumartalukdar@gmail.com>
2022-08-22 23:25:19 +0600

    function added

--- js/app.js
+++ js/app.js
@@ -1,14 +1,32 @@
-document.getElementById('btn-calculate').addEventListener('click', function () {
-    const perPlayerField = document.getElementById('input-field-per-player');
-    const newPerPlayerAmountString = perPlayerField.value;
-    const newPerPlayerAmount = parseInt(newPerPlayerAmountString);
+function inputFieldValues(element) {
+    const saleryField = document.getElementById(element);
+    const newSaleryString = saleryField.value;
+    const newSalery = parseInt(newSaleryString);
+    saleryField.value = '';
+    return newSalery;
 
-    perPlayerField.value = '';
+}
+document.getElementById('btn-calculate').addEventListener('click', function () {
+
+    const newPerPlayerAmount = inputFieldValues('input-field-per-player');
 
     const playerExpenseTotalElement = document.getElementById('player-expenses');
-    const previousPlayerExpenseTotalString = playerExpenseTotalElement.innerText;
-    const previousPlayerExpenseTotal = parseInt(previousPlayerExpenseTotalString);
 
     const newPlayerExpenseTotal = newPerPlayerAmount * 5;
     playerExpenseTotalElement.innerText = newPlayerExpenseTotal;
+});
+
+
+
+document.getElementById('btn-calculate-total').addEventListener('click', function () {
+    const newManagerSalery = inputFieldValues('input-field-manager');
+
+    const newCoachSalery = inputFieldValues('input-field-coach');
+
+    const playerExpenseTotalElement = document.getElementById('player-expenses');
+    const previousPlayerExpenseTotalString = playerExpenseTotalElement.innerText;
+    const newPlayerExpense = parseInt(previousPlayerExpenseTotalString);
+    const total = document.getElementById('total');
+    const newTotal = newPlayerExpense + newManagerSalery + newCoachSalery;
+    total.innerText = newTotal;
 })
